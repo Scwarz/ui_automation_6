dac91c3d568699776cc1579a789cf1e4e8bf2c63
Anatoli Kamyshev <Akamyshev93@gmail.com>
2023-06-10 15:09:23 -0500

    POM and Project2 refactoring

--- src/test/java/scripts/Base.java
+++ src/test/java/scripts/Base.java
@@ -4,12 +4,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import pages.GoogleSearchPage;
+import pages.GoogleSearchResultsPage;
+import pages.Project2Page;
 import utils.Driver;
 
 public class Base {
 
     public static WebDriver driver; // declaration
     public static Actions actions;
+
+    //Declaring pages
+    public static Project2Page project2Page;
+    public static GoogleSearchPage googleSearchPage;
+    public static GoogleSearchResultsPage googleSearchResultsPage;
 
     @BeforeMethod
     public void setUp(){
